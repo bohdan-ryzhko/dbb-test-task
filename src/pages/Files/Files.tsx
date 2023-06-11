8badbc1eb8a019cf786f7cfc56a3f9355600ed92
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-06-11 21:47:09 +0300

    add notification create new folder

--- src/pages/Files/Files.tsx
+++ src/pages/Files/Files.tsx
@@ -7,18 +7,31 @@ import { Title } from "../../components/Title/Title";
 import { ButtonCreateFolder } from "../../components/ButtonCreateFolder/ButtonCreateFolder";
 import { CreateFolderModal } from "../../components/CreateFolderModal/CreateFolderModal";
 import { RenderList } from "../../components/RenderList/RenderList";
+import { useLocation, useNavigate } from "react-router-dom";
+import { convertQueryStringToObject } from "../../utils/convertQueryStringToObject";
+import { toast } from "react-toastify";
 
 export const Files: FC = () => {
 	const dispatch: AppDispatch = useDispatch();
 	const { isAuth, folders, isLoad } = useAuth();
+	const { search } = useLocation();
+	const { create, newFolder } = convertQueryStringToObject(search);
+	const navigate = useNavigate();
 
 	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
 	useEffect(() => {
 		if (isAuth) {
 			dispatch(getUserInfo());
-		};
-	}, [dispatch, isAuth]);
+		}
+
+		if (create) {
+			toast.success(`${newFolder} added`);
+			navigate({ search: "" });
+			return;
+		}
+
+	}, [dispatch, isAuth, create]);
 
 	useEffect(() => {
 		if (isModalOpen) {
